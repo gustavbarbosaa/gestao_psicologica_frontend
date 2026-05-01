eeea32fc121c2d98f0020f2160c042627ed9d143
gustavbarbosaa <gustavoaraujohab@gmail.com>
2026-04-30 23:39:06 -0300

    remove: remove botao de login com google

--- src/app/features/login/pages/login/login.ts
+++ src/app/features/login/pages/login/login.ts
@@ -2,7 +2,6 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { EsqueceuSenha } from '@features/login/components/esqueceu-senha/esqueceu-senha';
 import { ZardButtonComponent } from '@shared/components/button/button.component';
 import { ZardCardComponent } from '@shared/components/card/card.component';
-import { ZardDividerComponent } from '@shared/components/divider/divider.component';
 import { generateId } from '@shared/utils/merge-classes';
 import { FormGroup, ReactiveFormsModule, NonNullableFormBuilder, Validators } from '@angular/forms';
 import { LoginService } from '@core/services/login-service';
@@ -10,45 +9,9 @@ import { Router } from '@angular/router';
 import { ToastService } from '@shared/services/toast-service';
 import { Logo } from '@shared/components/logo/logo';
 
-interface GoogleSignInResponse {
-  getBasicProfile(): {
-    getId(): string;
-    getName(): string;
-    getImageUrl(): string;
-    getEmail(): string;
-  };
-  getAuthResponse(): {
-    id_token: string;
-  };
-}
-
-interface GapiAuth2 {
-  init(options: { client_id: string }): Promise<void>;
-  getAuthInstance(): {
-    signIn(): Promise<GoogleSignInResponse>;
-    isSignedIn: {
-      get(): boolean;
-    };
-  };
-}
-
-interface Gapi {
-  load(name: string, callback: () => void): void;
-  auth2: GapiAuth2;
-}
-
-declare const gapi: Gapi;
-
 @Component({
   selector: 'app-login',
-  imports: [
-    ZardButtonComponent,
-    ZardCardComponent,
-    ZardDividerComponent,
-    EsqueceuSenha,
-    ReactiveFormsModule,
-    Logo,
-  ],
+  imports: [ZardButtonComponent, ZardCardComponent, EsqueceuSenha, ReactiveFormsModule, Logo],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
@@ -58,13 +21,12 @@ export class Login implements OnInit {
   protected loginForm!: FormGroup;
   protected loading = signal<boolean>(false);
 
-  private formBuilderService = inject(NonNullableFormBuilder);
-  private loginService = inject(LoginService);
-  private router = inject(Router);
-  private toastService = inject(ToastService);
+  private readonly formBuilderService = inject(NonNullableFormBuilder);
+  private readonly loginService = inject(LoginService);
+  private readonly router = inject(Router);
+  private readonly toastService = inject(ToastService);
 
   ngOnInit(): void {
-    this.initializeGoogleSignIn();
     this.initLoginForm();
   }
 
@@ -73,52 +35,6 @@ export class Login implements OnInit {
       email: ['', [Validators.email]],
       senha: ['', [Validators.minLength(6), Validators.maxLength(20)]],
     });
-  }
-
-  handleCredentialRespons(response: any): void {
-    const token = response.credential;
-    this.onSignIn(token);
-  }
-
-  private initializeGoogleSignIn(): void {
-    if (typeof gapi !== 'undefined') {
-      gapi.load('auth2', () => {
-        gapi.auth2.init({
-          client_id: '519340482200-2tl0nqoknnjnjfgnprgqkgvvrrbcrrpo.apps.googleusercontent.com',
-        });
-      });
-    }
-  }
-
-  signInWithGoogle(): void {
-    if (typeof gapi !== 'undefined' && gapi.auth2) {
-      try {
-        const auth2 = gapi.auth2.getAuthInstance();
-        auth2
-          .signIn()
-          .then((googleUser: GoogleSignInResponse) => {
-            this.onSignIn(googleUser);
-          })
-          .catch((error: Error) => {
-            console.error('Erro ao fazer login com Google:', error);
-          });
-      } catch (error) {
-        console.error('Erro ao obter instância de auth2:', error);
-      }
-    } else {
-      console.error('Google API não está disponível');
-    }
-  }
-
-  onSignIn(googleUser: GoogleSignInResponse): void {
-    const profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId());
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail());
-
-    const id_token = googleUser.getAuthResponse().id_token;
-    console.log('ID Token:', id_token);
   }
 
   protected signIn(): void {
