fd47efd1c8c7fd502b847f62177acb26940bdc56
gustavbarbosaa <gustavoaraujohab@gmail.com>
2026-05-23 22:43:54 -0300

    feat: refatora layout e estilos das páginas, adiciona lógica para menu mobile

--- src/app/shared/components/layout-pages/layout-pages.ts
+++ src/app/shared/components/layout-pages/layout-pages.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { LayoutComponent } from '../layout/layout.component';
 import { ContentComponent } from '../layout/content.component';
 import { SidebarComponent } from '../layout/sidebar.component';
@@ -6,10 +6,19 @@ import { Logo } from '../logo/logo';
 import { AvatarInfo } from '../avatar-info/avatar-info';
 import { MenuPrincipal } from '../menu-principal/menu-principal';
 import { LoginService } from '@core/services/login-service';
+import { ZardIconComponent } from '../icon/icon.component';
 
 @Component({
   selector: 'app-layout-pages',
-  imports: [LayoutComponent, ContentComponent, SidebarComponent, Logo, AvatarInfo, MenuPrincipal],
+  imports: [
+    LayoutComponent,
+    ContentComponent,
+    SidebarComponent,
+    Logo,
+    AvatarInfo,
+    MenuPrincipal,
+    ZardIconComponent,
+  ],
   templateUrl: './layout-pages.html',
   styleUrl: './layout-pages.css',
 })
@@ -17,8 +26,17 @@ export class LayoutPages implements OnInit {
   private loginService = inject(LoginService);
 
   usuario = this.loginService.usuario;
+  protected readonly menuMobileAberto = signal(false);
 
   ngOnInit(): void {
     this.loginService.carregarUsuario().subscribe();
+  }
+
+  protected abrirMenuMobile(): void {
+    this.menuMobileAberto.set(true);
+  }
+
+  protected fecharMenuMobile(): void {
+    this.menuMobileAberto.set(false);
   }
 }
